2fcb03d6bae1f26549738fcd0e6c3b48eeb8fba4
Isk On <ibrohim5285.qor1@gmail.com>
2023-05-16 18:18:32 +0500

    Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,165 +1,155 @@
 import React from 'react'
 import "../Footer/Footer.css"
 const Footer = () => {
-  return (
-    <div className='container'>
-        <div className="footer">
-            <div className="footer_items">
-<h2>Контакты</h2>
+    return (
+        <div className='container'>
+            <div className="footer">
+                <div className="footer_items">
+                    <h2>Контакты</h2>
 
-<div className="footer_item">
-            <i class="fa-sharp fa-solid fa-phone-volume"></i>
+                    <div className="footer_item">
+                        <i class="fa-sharp fa-solid fa-phone-volume"></i>
 
-                <h4>+7 951 159 1979</h4>
-            </div>
-            <div className="footer_item">
-            <i class="fa-sharp fa-solid fa-phone-volume"></i>
+                        <h4>+7 951 159 1979</h4>
+                    </div>
+                    <div className="footer_item">
+                        <i class="fa-sharp fa-solid fa-phone-volume"></i>
 
-                <h4>+7 951 159 1979</h4>
-            </div>
-            <div className="footer_item">
-            <i class="fa-sharp fa-solid fa-phone-volume"></i>
+                        <h4>+7 951 159 1979</h4>
+                    </div>
+                    <div className="footer_item">
+                        <i class="fa-sharp fa-solid fa-phone-volume"></i>
 
-                <h4>+7 951 159 1979</h4>
-            </div>
+                        <h4>+7 951 159 1979</h4>
+                    </div>
 
-            <div className="footer_item">
-            <i class="fa-regular fa-envelope"></i>
+                    <div className="footer_item">
+                        <i class="fa-regular fa-envelope"></i>
 
-                <h4>+7 951 159 1979</h4>
-            </div>
+                        <h4>+7 951 159 1979</h4>
+                    </div>
 
-            <div className="footer_item">
-            <i class="fa-solid fa-circle-xmark"></i>
+                    <div className="footer_item">
+                        <i class="fa-solid fa-circle-xmark"></i>
 
-                <h4>+7 951 159 1979</h4>
-            </div>
-            </div>
+                        <h4>+7 951 159 1979</h4>
+                    </div>
+                </div>
 
-            <div className="footer_items">
-            <h2>Контакты</h2>
-            <div className="footer_item">
-            <i class="fa-regular fa-circle"></i>
+                <div className="footer_items">
+                    <h2>О магазине
+                    </h2>
+                    <div className="footer_item">
+                        <i class="fa-regular fa-circle"></i>
 
-                <h4>Home</h4>
-            </div>
-            <div className="footer_item">
-            <i class="fa-regular fa-circle"></i>
+                        <h4>Home</h4>
+                    </div>
+                    <div className="footer_item">
+                        <i class="fa-regular fa-circle"></i>
 
-                <h4>About</h4>
-            </div>
-            <div className="footer_item">
-            <i class="fa-regular fa-circle"></i>
+                        <h4>About</h4>
+                    </div>
+                    <div className="footer_item">
+                        <i class="fa-regular fa-circle"></i>
 
-                <h4>All Products</h4>
-            </div>
+                        <h4>All Products</h4>
+                    </div>
 
-            <div className="footer_item">
-            <i class="fa-regular fa-circle"></i>
+                    <div className="footer_item">
+                        <i class="fa-regular fa-circle"></i>
 
-                <h4>Contact</h4>
-            </div>
+                        <h4>Contact</h4>
+                    </div>
 
-         
+
                 </div>
 
 
                 <div className="footer_items">
-                <h2>Контакты</h2>
-                <div className="footer_item">
-            <i class="fa-sharp fa-solid fa-phone-volume"></i>
+                    <h2>Каталог товаров</h2>
+                    <div className="footer_item">
+                        <i class="fa-regular fa-circle-check"></i>
 
-                <h4>+7 951 159 1979</h4>
-            </div>
-            <div className="footer_item">
-            <i class="fa-sharp fa-solid fa-phone-volume"></i>
+                        <h4>+7 951 159 1979</h4>
+                    </div>
+                    <div className="footer_item">
+                        <i class="fa-regular fa-circle-check"></i>
 
-                <h4>+7 951 159 1979</h4>
-            </div>
-            <div className="footer_item">
-            <i class="fa-sharp fa-solid fa-phone-volume"></i>
+                        <h4>+7 951 159 1979</h4>
+                    </div>
+                    <div className="footer_item">
+                        <i class="fa-regular fa-circle-check"></i>
 
-                <h4>+7 951 159 1979</h4>
-            </div>
+                        <h4>+7 951 159 1979</h4>
+                    </div>
 
-            <div className="footer_item">
-            <i class="fa-regular fa-envelope"></i>
+                    <div className="footer_item">
+                        <i class="fa-regular fa-circle-check"></i>
 
-                <h4>+7 951 159 1979</h4>
-            </div>
+                        <h4>+7 951 159 1979</h4>
+                    </div>
 
-            <div className="footer_item">
-            <i class="fa-solid fa-circle-xmark"></i>
-
-                <h4>+7 951 159 1979</h4>
-            </div>
+                    <div className="footer_item">
+                        <i class="fa-regular fa-circle-check"></i>
+                        <h4>+7 951 159 1979</h4>
+                    </div>
                 </div>
-                
+
 
                 <div className="footer_items">
-                <h2>Контакты</h2>
-                <div className="footer_item">
-            <i class="fa-sharp fa-solid fa-phone-volume"></i>
+                    <h2>Подписка</h2>
+                    <div className="footer_item">
+                        <i class="fa-sharp fa-solid fa-phone-volume"></i>
 
-                <h4>+7 951 159 1979</h4>
-            </div>
-            <div className="footer_item">
-            <i class="fa-sharp fa-solid fa-phone-volume"></i>
+                        <h4>+7 951 159 1979</h4>
+                    </div>
+                    <div className="footer_item">
+                        <i class="fa-sharp fa-solid fa-phone-volume"></i>
 
-                <h4>+7 951 159 1979</h4>
-            </div>
-            <div className="footer_item">
-            <i class="fa-sharp fa-solid fa-phone-volume"></i>
+                        <h4>+7 951 159 1979</h4>
+                    </div>
+                    <div className="footer_item">
+                        <i class="fa-sharp fa-solid fa-phone-volume"></i>
 
-                <h4>+7 951 159 1979</h4>
-            </div>
+                        <h4>+7 951 159 1979</h4>
+                    </div>
 
-            <div className="footer_item">
-            <i class="fa-regular fa-envelope"></i>
+                    <div className="footer_item">
+                        <i class="fa-regular fa-envelope"></i>
 
-                <h4>+7 951 159 1979</h4>
-            </div>
+                        <h4>+7 951 159 1979</h4>
+                    </div>
 
-            <div className="footer_item">
-            <i class="fa-solid fa-circle-xmark"></i>
+                    <div className="footer_item">
+                        <i class="fa-solid fa-circle-xmark"></i>
 
-                <h4>+7 951 159 1979</h4>
-            </div>
+                        <h4>+7 951 159 1979</h4>
+                    </div>
                 </div>
 
-                <div className="footer_items">
-                <h2>Контакты</h2>
-                <div className="footer_item">
-            <i class="fa-sharp fa-solid fa-phone-volume"></i>
-
-                <h4>+7 951 159 1979</h4>
             </div>
-            <div className="footer_item">
-            <i class="fa-sharp fa-solid fa-phone-volume"></i>
+            <hr />
 
-                <h4>+7 951 159 1979</h4>
-            </div>
-            <div className="footer_item">
-            <i class="fa-sharp fa-solid fa-phone-volume"></i>
-
-                <h4>+7 951 159 1979</h4>
-            </div>
-
-            <div className="footer_item">
-            <i class="fa-regular fa-envelope"></i>
-
-                <h4>+7 951 159 1979</h4>
-            </div>
-
-            <div className="footer_item">
-            <i class="fa-solid fa-circle-xmark"></i>
-
-                <h4>+7 951 159 1979</h4>
-            </div>
+            <div className="footer_bottom">
+                <div className="left">
+<h3>© 2023
+Интернет-магазин </h3>
                 </div>
+
+                <div className="right">
+<h3>Created by Ibroxim Developer</h3>
+                </div>
+            </div>
+
+
+
+
+
+
+
+
         </div>
-    </div>
-  )
+    )
 }
 
 export default Footer
