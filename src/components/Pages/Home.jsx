277796fcb6ff2d79b85e48030f1ab3baf18b4418
Isk On <ibrohim5285.qor1@gmail.com>
2023-05-18 17:59:50 +0500

    cccc

--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -6,13 +6,14 @@ import Footer from '../Footer/Footer'
 import Section from '../Section/Section'
 import Contact from '../Contacts/Contact'
 import CategCard from '../CategCard/CategCard'
-
+import Navbar1 from '../Navbar1/Navbar1'
 import Doma from "../Products/Doma/Dome"
 const Home = () => {
     return (
         <>
         <TopNav/>
             <Navbar />
+            {/* <Navbar1/> */}
            <Swiper/>
            <Section/>
            <Contact/>
