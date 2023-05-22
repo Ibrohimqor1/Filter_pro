f009932ac9482fcb0f24d9083be4d05f53a43756
Ibrohimqor1 <115447690+Ibrohimqor1@users.noreply.github.com>
2023-05-22 12:49:27 +0500

    ss

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,38 +1,34 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
-import "../Header/Header.css"
+
 // Import Swiper styles
 import "swiper/css";
-import "swiper/css/navigation";
 import "swiper/css/pagination";
 
-
+import "./styles.css";
 
 // import required modules
-import { Navigation, Pagination, Mousewheel, Keyboard } from "swiper";
+import { Pagination } from "swiper";
 
 export default function App() {
   return (
     <>
       <Swiper
-        cssMode={true}
-        navigation={true}
-        pagination={true}
-        mousewheel={true}
-        keyboard={true}
-        modules={[Navigation, Pagination, Mousewheel, Keyboard]}
+        spaceBetween={30}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Pagination]}
         className="mySwiper"
       >
-         <SwiperSlide><img src="https://d2evkimvhatqav.cloudfront.net/images/_800x418_crop_center-center_82_none/children_water_126439930.jpg?v=1683291228" alt="" /></SwiperSlide>
-        <SwiperSlide><img src="https://d2evkimvhatqav.cloudfront.net/images/_800x418_crop_center-center_82_none/children_water_126439930.jpg?v=1683291228" alt="" /></SwiperSlide>
+        <SwiperSlide><img src="https://dpw4tdh0of7va.cloudfront.net/wp-content/uploads/2020/01/family-drinking-water.jpg" alt="" /></SwiperSlide>
+        <SwiperSlide><img src="https://dpw4tdh0of7va.cloudfront.net/wp-content/uploads/2020/01/family-drinking-water.jpg" alt="" /></SwiperSlide>
+        <SwiperSlide><img src="https://dpw4tdh0of7va.cloudfront.net/wp-content/uploads/2020/01/family-drinking-water.jpg" alt="" /></SwiperSlide>
+        <SwiperSlide><img src="https://dpw4tdh0of7va.cloudfront.net/wp-content/uploads/2020/01/family-drinking-water.jpg" alt="" /></SwiperSlide>
        
-       
-        <SwiperSlide><img src="http://filter-shop.kz/files/resized/slides/avtomaty.1067x400.center.center.png.webp" alt="" /></SwiperSlide>
-        <SwiperSlide><img src="http://filter-shop.kz/files/resized/slides/filtruyuschie.1067x400.center.center.png.webp" alt="" /></SwiperSlide>
-        
-   
       </Swiper>
     </>
   );
 }
+
