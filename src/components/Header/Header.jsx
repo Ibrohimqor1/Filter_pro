669e17b8283b7930881cdad9aec3d729bae32b92
Ibrohimqor1 <115447690+Ibrohimqor1@users.noreply.github.com>
2023-05-22 14:28:42 +0500

    salom

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,34 +1,9 @@
-import React, { useRef, useState } from "react";
-// Import Swiper React components
-import { Swiper, SwiperSlide } from "swiper/react";
+import React from 'react'
 
-// Import Swiper styles
-import "swiper/css";
-import "swiper/css/pagination";
-
-import "./styles.css";
-
-// import required modules
-import { Pagination } from "swiper";
-
-export default function App() {
+const Header = () => {
   return (
-    <>
-      <Swiper
-        spaceBetween={30}
-        pagination={{
-          clickable: true,
-        }}
-        modules={[Pagination]}
-        className="mySwiper"
-      >
-        <SwiperSlide><img src="https://dpw4tdh0of7va.cloudfront.net/wp-content/uploads/2020/01/family-drinking-water.jpg" alt="" /></SwiperSlide>
-        <SwiperSlide><img src="https://dpw4tdh0of7va.cloudfront.net/wp-content/uploads/2020/01/family-drinking-water.jpg" alt="" /></SwiperSlide>
-        <SwiperSlide><img src="https://dpw4tdh0of7va.cloudfront.net/wp-content/uploads/2020/01/family-drinking-water.jpg" alt="" /></SwiperSlide>
-        <SwiperSlide><img src="https://dpw4tdh0of7va.cloudfront.net/wp-content/uploads/2020/01/family-drinking-water.jpg" alt="" /></SwiperSlide>
-       
-      </Swiper>
-    </>
-  );
+    <div>Header</div>
+  )
 }
 
+export default Header
