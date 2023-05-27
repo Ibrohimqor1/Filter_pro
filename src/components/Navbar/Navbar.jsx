3d8572752a22d975f76e941b4605998d6a452687
abdumom1nov1cho1 <norqulovanvar105@gmail.com>
2023-05-27 22:56:39 +0500

    sss
    sign in vs sign up

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import "../Navbar/Navbar.css"
-import { Link } from 'react-router-dom'
+import React from "react";
+import "../Navbar/Navbar.css";
+import { Link } from "react-router-dom";
 const Navbar = () => {
   React.useEffect(() => {
     window.scrollTo({
@@ -13,33 +13,52 @@ const Navbar = () => {
       <div className="navbar">
         <div className="left">
           <Link to="/">
-
-            <img src="https://homefilter.kz/assets/images/common/logo_black.svg" alt="" />
+            <img
+              src="https://homefilter.kz/assets/images/common/logo_black.svg"
+              alt=""
+            />
           </Link>
         </div>
         <div className="center">
           <ul>
-            <li><a href="/">Home</a></li>
-            <Link to="/about"><a href="">About</a></Link>
-            <li><a href="">Home</a></li>
-            <li><a href="">Home</a></li>
-            <Link to="/admin">
-              <li><a href="">Admin</a></li>
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <Link to="/about">
+              <a href="">About</a>
             </Link>
-
+            <li>
+              <a href="">Home</a>
+            </li>
+            <li>
+              <a href="">Home</a>
+            </li>
+            <Link to="/admin">
+              <li>
+                <a href="">Admin</a>
+              </li>
+            </Link>
           </ul>
         </div>
 
         <div className="right">
           <Link to="/korzinka">
             <h4>
-              <img className='icon' src="https://www.ibu.paris/modules/blockreassurance/views/img/img_perso/caddiee.png" alt="" /></h4>
+              <img
+                className="icon"
+                src="https://www.ibu.paris/modules/blockreassurance/views/img/img_perso/caddiee.png"
+                alt=""
+              />
+            </h4>
           </Link>
-
+          <div className="sign__btns">
+            <button className="sign__up__btn">Sign Up</button>
+            <button className="sign__up__btn">Sign In</button>
+          </div>
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
