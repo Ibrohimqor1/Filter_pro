1630310bffe85c84370612118d611cab85d78bf5
ibrohimqor1 <ibrohim5285.qor1@gmail.com>
2023-06-03 18:47:18 +0500

    About page

--- src/components/About/about.jsx
+++ src/components/About/about.jsx
@@ -8,8 +8,35 @@ const about = () => {
     <Top_Nav/>
 <Navbar/>
 
-<h1>About us</h1>
-<h3>About</h3>
+<div class="div">
+    <h1 class="h">Ремонт, установка фильтров для воды</h1>
+    <h1 class="h1">Установка, ремонт, замена запасных частей фильтров для воды c выездом по городу Бишкек.
+</h1>
+</div>
+<div class="container">
+<div class="cards">
+    <div class="card">
+        <img src="https://delux.kg/wp-content/uploads/2020/07/2.png" alt="">
+        <h2>Диагностика и замена картриджей</h2>
+        <h3>Диагностика фильтра для воды, <br> замена сменных картриджей </h3>
+    </div>
+    <div class="card">
+        <img src="https://delux.kg/wp-content/uploads/2020/07/3-1.png" alt="">
+        <h2>Несложный ремонт фильтра</h2>
+        <h3>Вызов мастера (замена фитингов, смесителя, замена колбы)</h3>
+    </div>
+    <div class="card">
+        <img src="https://delux.kg/wp-content/uploads/2020/07/4.png" alt="">
+        <h2>Сложный ремонт</h2>
+        <h3>Восстановление и модернизация фильтров для воды на дому</h3>
+    </div>
+    <div class="card">
+        <img src="https://delux.kg/wp-content/uploads/2020/07/5.png" alt="">
+        <h2>Установка фильтра</h2>
+        <h3>Монтаж и установка фильтров для воды под мойку</h3>
+    </div>
+</div>
+</div>
 
 
 
