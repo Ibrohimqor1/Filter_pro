68557425d49be953c38fba034e682f099754fe67
abdumom1nov1cho1 <norqulovanvar105@gmail.com>
2023-06-11 09:34:14 +0500

    9:33 11.06.2023

--- src/components/About/about.jsx
+++ src/components/About/about.jsx
@@ -16,22 +16,22 @@ const about = () => {
 <div class="container">
 <div class="cards">
     <div class="card">
-        <img src="https://delux.kg/wp-content/uploads/2020/07/2.png" alt="">
+        <img src="https://delux.kg/wp-content/uploads/2020/07/2.png" alt=""/>
         <h2>Диагностика и замена картриджей</h2>
-        <h3>Диагностика фильтра для воды, <br> замена сменных картриджей </h3>
+        <h3>Диагностика фильтра для воды, <br/> замена сменных картриджей </h3>
     </div>
     <div class="card">
-        <img src="https://delux.kg/wp-content/uploads/2020/07/3-1.png" alt="">
+        <img src="https://delux.kg/wp-content/uploads/2020/07/3-1.png" alt=""/>
         <h2>Несложный ремонт фильтра</h2>
         <h3>Вызов мастера (замена фитингов, смесителя, замена колбы)</h3>
     </div>
     <div class="card">
-        <img src="https://delux.kg/wp-content/uploads/2020/07/4.png" alt="">
+        <img src="https://delux.kg/wp-content/uploads/2020/07/4.png" alt=""/>
         <h2>Сложный ремонт</h2>
         <h3>Восстановление и модернизация фильтров для воды на дому</h3>
     </div>
     <div class="card">
-        <img src="https://delux.kg/wp-content/uploads/2020/07/5.png" alt="">
+        <img src="https://delux.kg/wp-content/uploads/2020/07/5.png" alt=""/>
         <h2>Установка фильтра</h2>
         <h3>Монтаж и установка фильтров для воды под мойку</h3>
     </div>
